868c722b2d126a78c4553832a1c30ba2c9a11df1
James Swineson <jamesswineson@gmail.com>
2016-09-27 12:44:06 +0800

    Eliminate warnings

--- examples/UDP_sample/client.c
+++ examples/UDP_sample/client.c
@@ -24,13 +24,13 @@ int main(int argc, char *argv[])
 		char recvBuff[1024] = {0};
 		printf("Send >");
 #ifdef _WIN32
-		scanf_s("%1023s", data, sizeof(data));
+		scanf_s("%1023s", data, 1024);
 #else
 		scanf("%1023s", data);
 #endif
 		if(strcmp("quit", data)==0)
 			break;
-		sendto(sConnection, data, strlen(data)+1, 0, peer, NSLEndpointV4SocketLen);
+		sendto(sConnection, data, (int)strlen(data)+1, 0, peer, NSLEndpointV4SocketLen);
 		socklen_t address_len = NSLEndpointV4SocketLen;
 		recvfrom(sConnection, recvBuff, 1024, 0, peer, &address_len);
 		printf("Received: %s\n", recvBuff);
