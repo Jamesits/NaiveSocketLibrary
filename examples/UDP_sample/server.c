f25379e6fc4107250d72786768ed1dd7f1097e38
James Swineson <i@swineson.me>
2017-04-21 23:08:36 +0800

    code changes

--- examples/UDP_sample/server.c
+++ examples/UDP_sample/server.c
@@ -1,4 +1,4 @@
-/* 
+/*
 	NaiveSocketLibrary UDP test server
 	Listens on 0.0.0.0:5150
 	Echos everything sent from client.
@@ -11,9 +11,11 @@
 
 int main(int argc, char *argv[])
 {
+    static int so_broadcast = TRUE;
    	NSLInit();
 	SOCKET sListen = NSLSocket(AF_INET, SOCK_DGRAM, 0);
 	SOCKADDR *conn = NSLEndpointV4("0.0.0.0", 5150);
+    setsockopt(s, SOL_SOCKET, SO_BROADCAST, &so_broadcast, sizeof so_broadcast);
 	NSLBindV4(sListen, conn);
 	printf("binded, press ^C to quit...\n");
 
@@ -21,17 +23,17 @@ int main(int argc, char *argv[])
 	{
 		SOCKADDR_IN clientAddr;
 		memset(&clientAddr, 0, sizeof(clientAddr));
-				
+
 		char message[1024] = {0};
 		socklen_t address_len = NSLEndpointV4SocketLen;
 		int recvBytes = recvfrom(sListen, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
-		if(recvBytes <= 0) 
+		if(recvBytes <= 0)
 		{
 			// non-blocking receive will return a -1
 			break;
 		}
 		printf("Received: %s\n", message);
-		sendto(sListen, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len); 
+		sendto(sListen, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
 	}
 
    NSLCloseSocket(sListen);
